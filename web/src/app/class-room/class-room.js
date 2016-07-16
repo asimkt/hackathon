2a9cbb987ddee617c40d4cbee8ed544da9e9f6b1
Asim KT <aasimkt@gmail.com>
2016-07-16 10:00:38 +0530

    [Video autoload]

--- web/src/app/class-room/class-room.js
+++ web/src/app/class-room/class-room.js
@@ -6,15 +6,37 @@ angular
   });
 
 /** @ngInject */
-function CourseController(ngDialog) {
+function CourseController(ngDialog, $scope) {
   var vm = this;
 
   vm.startTest = function(){
     ngDialog.open({
-        template: 'app/class-room/user-evaluation-dialog.html'
+        template: 'app/class-room/user-evaluation-dialog.html',
+        scope: $scope
     });
     // ngDialog.open({ template: 'userEvaluationDialog', className: 'ngdialog-theme-default' });
   }
+  vm.class_room = {
+    video_data: {
+      link1: 'http://vjs.zencdn.net/v/oceans.mp4',
+      link2: 'http://vjs.zencdn.net/v/oceans.webm'
+    }
+  };
+  // vm.startTest();
+  $scope.evaluate = function(data){
+    console.log('evaluating', data);
+    var point = 0;
+    if(data.ans1 === 'yes') 
+      point++;
+    if(data.ans2 === 'no') 
+      point++;
+    if(data.ans3 === 'yes') 
+      point++;
+
+    $scope.$broadcast('evaluate', { point: point });
+    ngDialog.closeAll();
+  }
+
   vm.courses = [
     { "name": "Python", "description": "This is a programming course" },
     { "name": "Angular Islands", "description": "This is a programming course" },
--- web/src/app/class-room/video-player.js
+++ web/src/app/class-room/video-player.js
@@ -5,5 +5,36 @@ angular
     bindings: {
       video: '<',
       onVideoEnded: '&'
-    }
+    },
+    controller: videoPlayerController
   });
+
+  /** @ngInject */
+function videoPlayerController($timeout, $scope) {
+  var vm = this;
+  console.log('initlazd');
+  $scope.$on('evaluate', function (event, args) {
+	   $scope.point = args.point;
+	   console.log($scope.point);
+       var video = document.getElementsByTagName('video')[0];
+       var sources = video.getElementsByTagName('source');
+	   switch($scope.point) {
+	     case 1:
+	       console.log('play 1');
+           sources[0].src = 'https://embedwistia-a.akamaihd.net/deliveries/adc728d580c8a52275278697d19e1e9f7526f157/file.mp4';
+           video.load();
+	       break;
+	     case 2:
+	       console.log('play 2');
+	       break;
+	     case 3:
+	       console.log('play 3');
+	       break;
+	     default:
+	       console.log('play 4');
+	       sources[0].src = 'http://clips.vorwaerts-gmbh.de/VfE_html5.mp4';
+           video.load();
+	       break;
+	   }
+   });
+}
